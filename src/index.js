a84ffc88e450609a1be45a6f43e4fa033403f638
PolinaKravchuk <polina8kravchuk@gmail.com>
2018-02-27 15:55:16 +0300

    features/number in chislo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  	var kol_5_5 = 1,
+	    kol = 0,
+	    mass = [],
+	    sum = 0,
+	    t,
+	    c,
+	    v,
+	    num = number;
+
+	while (c != 1){
+		
+		ost  = num % base;
+		c = num / base - ost;
+			chislo.push(ost);
+	}
+
+	for (var i = 0; i < chislo.length / 2; i++){
+		v = chislo[i];
+		chislo[i] = chislo[length-i-1];
+		chislo[length-i-1] = v;
+	}
+	var ch = chislo.join('');
+	while (kol_5_5 < ch){
+		kol_5_5 = kol_5_5 * 5;
+		mass.push (kol_5_5);
+	}
+
+	for (var i = 0; i < mass.length; i++){
+		t = number / mass[i];
+		t = Math.floor (t);
+		sum = sum + t;
+	}
+
+	return sum;
+} 
