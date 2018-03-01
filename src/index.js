0ceda94026ace0233590556662c2abd985806569
Polina Kravchuk <34372082+PolinaKravchuk@users.noreply.github.com>
2018-03-01 19:56:24 +0300

    new project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+ var kol_5_5 = 1, mass = [], sum = 0, c = 300, ost, v = "", str = "", t, chislo;
+  while (c >= base){
+		ost  = number % base;
+		c = (number - ost) / base;
+		v = String(ost);
+		str = v + str;
+	}
+	
+	str = str + c;
+	console.log(str);
+	str = str.reverse();
+	console.log(str);
+	chislo = Number(str);
+	console.log(chislo);
+
+	while (kol_5_5 < chislo){
+		kol_5_5 = kol_5_5 * 5;
+		mass.push (kol_5_5);
+	}
+
+	for (var i = 0; i < mass.length; i++){
+		t = chislo / mass[i];
+		t = Math.floor (t);
+		sum = sum + t;
+	}
+
+	return sum;
 }
