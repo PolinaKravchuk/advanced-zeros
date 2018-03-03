3c9c82adf89170f82029a868ef8e742acb8680f4
Polina Kravchuk <34372082+PolinaKravchuk@users.noreply.github.com>
2018-03-03 13:43:40 +0300

    feature/ charAt

--- src/index.js
+++ src/index.js
@@ -1,54 +1,43 @@
 module.exports =
  function getZerosCount(number, base) {
-  	var kol_5_5 = 1, mass = [], sum = 0,
+  		var kol_5_5 = 1, mass = [], sum = 0,
   		c = number, ost, v , str = "", t, chislo;
   		var arr = [];
   			while (true) {
   				if (c < base) {
-					console.log(c);
 					break;
 				}
 				ost  = c % base;
-				console.log(ost);
 
 				c = (c - ost) / base;
 				c = Math.floor (c);
-				console.log(c);
 
-				//v = ost.toString();
-				//console.log(v);
+				v = ost.toString();
 
-				arr.push(ost);
-
-				console.log(str);
+				arr.push(v);
 				
 		
 			}
 							
-	//c = c.toString();	 
+	c = c.toString();	 
 	arr.push(c);	
-	console.log(arr);
-	console.log(arr[0]);
-	console.log(arr[3]);
-	/*var tr;
-	tr = str + c;
-	//console.log(typeOf(tr));
-	console.log(tr);
-	
-	console.log(arr);*/
-	//tr.reverse();
-	var s = [];
 	arr.reverse();
-	/*for (var i = 0; i < arr.length; i++){
-		s.push(arr[i]);
-		s[i].reverse();
-		console.log(s);
-	}*/
+
+	var s;
+	
+	for (var i = 0; i < arr.length; i++){
+		s = arr[i];
+		var newstr = '', j;
+	    for (j = s.length - 1; j >= 0; j--) {
+	        newstr += s.charAt(j);
+	    }
+	    arr[i] = newstr;
+		
+	}
+
 	tr = arr.join('');
-	console.log(tr);
 
 	chislo = Number(tr);
-	console.log(chislo);
 
 	while (kol_5_5 < chislo){
 		kol_5_5 = kol_5_5 * 5;
@@ -63,6 +52,3 @@ module.exports =
 
 	return sum;
 } 
-
-//getZerosCount (46899647, 232);
-//console.log(k);
